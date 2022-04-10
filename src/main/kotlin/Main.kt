020ea519acf4014dd469ab5047b6d4b58c994f9b
Vojta <januvojta@gmail.com>
2022-04-10 12:14:49 +0200

    manually tested FutureSummary.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,12 +7,12 @@ import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 
 fun main(args: Array<String>) {
-    val url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=BRLKRW=X"
+    val url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=ALI=F"
     runBlocking {
         val text = getHtml(url)
         val obj =
             text.toJsonElement().jsonObject["quoteResponse"]?.jsonObject?.get("result")?.jsonArray?.get(0).toString()
-                .toJson(CurrencySummary::class)
+                .toJson(FutureSummary::class)
         println("name: ${obj.shortName}")
 
     }
